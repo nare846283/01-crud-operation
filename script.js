bdb7ef0ac4a4bbbaf84ad96cebba8847537a5730
Narendra Ahirwar <71281553+nare846283@users.noreply.github.com>
2025-03-04 17:25:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let submitBtn = document.querySelector("#submitBtn");
 
 function setLocalStorage() {
+  let showDiv = document.querySelector("#show");
+  showDiv.innerHTML = "";
+
   if (localStorage.getItem("userData")) {
-    let showDiv = document.querySelector("#show");
-    showDiv.innerHTML = "";
-    let arr = JSON.parse(localStorage.getItem("userData"));
+    let arr = JSON.parse(localStorage.getItem("userData")) || [];
+
     arr.forEach((user, id) => {
       let newDiv = document.createElement("div");
       newDiv.setAttribute("class", "newData");
@@ -12,98 +14,84 @@ function setLocalStorage() {
       let htmldata = `Me:- <span>${user.name}</span>
                       Password:- <span>${user.password}</span>
                       <button onClick='onDelete(${id})'> Delete </button>
-                      <button id="btnEdit" onClick='onEdit(${id})'> Edit </button>`;
+                      <button id="btnEdit-${id}" onClick='onEdit(${id})'> Edit </button>`;
+
       newDiv.insertAdjacentHTML("afterbegin", htmldata);
       showDiv.insertAdjacentElement("afterbegin", newDiv);
     });
   } else {
-    let arr = [];
-    let arrData = {
-      name: "",
-      password: "",
-    };
-    arr.push(arrData);
-    localStorage.setItem(userData, JSON.stringify(arr));
-    alert("data pushed");
+    localStorage.setItem("userData", JSON.stringify([]));
   }
 }
 
-setTimeout(() => {
-  setLocalStorage();
-}, 2);
+// Delay ke bina directly call karein
+setLocalStorage();
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let arr = JSON.parse(localStorage.getItem("userData"));
   let name = document.querySelector("#name").value;
   let password = document.querySelector("#password").value;
 
-  if (name.length <= 0 && password.length <= 0) {
+  if (!name || !password) {
     alert("Kuch enter karein");
-  } else if (name.length > 0 && password.length > 0) {
-    let arrData = {
-      name: name,
-      password: password,
-    };
-
-    // if (!arr) {
-    //   arr = [];
-    // }
-
-    arr.push(arrData);
-    localStorage.setItem("userData", JSON.stringify(arr));
-    setLocalStorage();
-    alert("Successfully add kiya gaya");
-  } else {
-    alert("Kuch enter karein");
+    return;
   }
-});
 
-// delete function
+  let arr = JSON.parse(localStorage.getItem("userData")) || [];
 
-function onDelete(id) {
-  let arr = JSON.parse(localStorage.getItem("userData"));
-  let deleteArr = [...arr];
-  deleteArr.splice(id, 1);
-  arr = [...deleteArr];
+  let arrData = { name, password };
+  arr.push(arrData);
+
   localStorage.setItem("userData", JSON.stringify(arr));
   setLocalStorage();
+  document.querySelector("#name").value = "";
+  document.querySelector("#password").value = "";
+  alert("Successfully add kiya gaya");
+});
 
-  alert("Data Deleted")
+// Delete function
+function onDelete(id) {
+  let arr = JSON.parse(localStorage.getItem("userData")) || [];
+  arr.splice(id, 1);
+  localStorage.setItem("userData", JSON.stringify(arr));
+  setLocalStorage();
+  alert("Data Deleted");
 }
 
-// update function
-
+// Update function
 function onEdit(id) {
-  let arr = JSON.parse(localStorage.getItem("userData"));
-  let name = (document.querySelector("#name").value = arr[id].name);
-  let password = (document.querySelector("#password").value = arr[id].password);
+  let arr = JSON.parse(localStorage.getItem("userData")) || [];
+  let nameInput = document.querySelector("#name");
+  let passwordInput = document.querySelector("#password");
+
+  nameInput.value = arr[id].name;
+  passwordInput.value = arr[id].password;
 
   submitBtn.setAttribute("disabled", true);
 
-  let editBtn = document.createElement("button");
   let form = document.querySelector("form");
-  let btnEdit = document.querySelectorAll("#btnEdit");
-  editBtn.innerHTML = "Update";
+  let existingEditBtn = document.querySelector("#updateBtn");
 
-  btnEdit.forEach((elements) => {
-    elements.setAttribute("disabled", true);
-    
-  });
+  if (!existingEditBtn) {
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = "Update";
+    editBtn.id = "updateBtn";
+    form.appendChild(editBtn);
 
-  form.insertAdjacentElement("beforeend", editBtn);
+    editBtn.addEventListener("click", (e) => {
+      e.preventDefault();
 
-  editBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let newName = document.querySelector("#name");
-    let newPassword = document.querySelector("#password");
-    arr.splice(id, 1, { name: newName.value, password: newPassword.value });
-    localStorage.setItem("userData", JSON.stringify(arr));
-    setLocalStorage();
-    newName.value = "";
-    newPassword.value = "";
-    form.removeChild(form.lastElementChild);
-    submitBtn.removeAttribute("disabled");
-    alert("Data Updated")
-  });
+      arr[id] = { name: nameInput.value, password: passwordInput.value };
+      localStorage.setItem("userData", JSON.stringify(arr));
+
+      nameInput.value = "";
+      passwordInput.value = "";
+
+      submitBtn.removeAttribute("disabled");
+      form.removeChild(editBtn);
+
+      setLocalStorage();
+      alert("Data Updated");
+    });
+  }
 }
